9bef81d654cac3542c9de54bc888aa9e5a4240d0
Brian Salemi <briandsalemi@gmail.com>
2019-04-21 11:58:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,5 +9,12 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(int)
+def countdown_with_sleep(number)
+  number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep 1
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
